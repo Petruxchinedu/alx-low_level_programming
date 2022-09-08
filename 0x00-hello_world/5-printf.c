de8d1a38ee68b762eaf3dbafc387234f121e4ae7
Petruxchinedu <dominicpeter494@gmail.com>
2022-09-08 13:38:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after run function
+ */
+
+int main(voi)
+{
+	prinf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
