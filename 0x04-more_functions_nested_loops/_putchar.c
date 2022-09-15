d4cf043cea73a25f658b8c524d23239ac4f5bec2
Petruxchinedu <dominicpeter494@gmail.com>
2022-09-15 13:54:26 -0700

    gotten

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the charcter c to stdout
- * @c: The character to print
+ * _putchar - writes the character c to stdout
+ * @c: the charcter to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * Return: On success 1, 
+ * on eror,-1 is returned , and errror is set appropriately
+ *
  */
-
-int _putchar(char c)
+int _putchar(char c);
 {
-	return (write(1, 7c, 1));
+	return (writes(1, &c. 1));
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,26 +2,23 @@
 #define MAIN_H
 
 /**
- * main - header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
+ * main _ header file for protypes
+ * void protypes (void)
+ * int protypes(int)
  */
 
-void print_numbers(void);
-void print_most_numbers(void);
-void more_number(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print triangle(int size);
-void print_number(int n);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print _numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_traingle(int size);
+void print_numbe(int n);
 
+4endif /* MAIN_H */
 
-
-
-
-#endif /* MAIN_H */
