759f4502233055780bd1b91e75f0110dc61202fe
Petruxchinedu <dominicpeter494@gmail.com>
2022-09-15 03:55:01 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Author - Peter Igbo
+ * Description - header contain prototype of function used
+ * in 0x04-more_functions_nested_loops
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+#endif /* MAIN_H */
